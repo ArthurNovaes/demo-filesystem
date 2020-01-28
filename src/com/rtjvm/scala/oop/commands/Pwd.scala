1a55d26628d04bc906dcfc1e02ac8dab6df80ede
Arthur Novaes <arthurnovaes9@gmail.com>
2020-01-28 14:36:06 -0300

    feat: add pwd command
    Signed-off-by: Arthur Novaes <arthurnovaes9@gmail.com>

---
+++ src/com/rtjvm/scala/oop/commands/Pwd.scala
@@ -0,0 +1,8 @@
+package com.rtjvm.scala.oop.commands
+import com.rtjvm.scala.oop.filesystem.State
+
+class Pwd extends Command {
+
+  override def apply(state: State): State =
+    state.setMessage(state.wd.path)
+}
---
+++ src/com/rtjvm/scala/oop/filesystem/FileSystemException.scala
@@ -0,0 +1,5 @@
+package com.rtjvm.scala.oop.filesystem
+
+class FileSystemException(message: String) extends RuntimeException(message) {
+
+}
