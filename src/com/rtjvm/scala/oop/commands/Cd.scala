843f851667eda6799ab6f63f541675d285950e2f
Arthur Novaes <arthurnovaes9@gmail.com>
2020-01-30 16:34:03 -0300

    feat: finish relative and absolute path for cd command feat: add collapse feature on cd
    Signed-off-by: Arthur Novaes <arthurnovaes9@gmail.com>

--- src/com/rtjvm/scala/oop/commands/Cd.scala
+++ src/com/rtjvm/scala/oop/commands/Cd.scala
@@ -48,10 +48,34 @@ class Cd(dir: String) extends Command {
       }
     }
 
+    @tailrec
+    def collapseRelativeTokens(path: List[String], result: List[String]): List[String] = {
+      if (path.isEmpty) result
+      else if(".".equals(path.head)) collapseRelativeTokens(path.tail, result)
+      else if("..".equals(path.head)) {
+        if (result.isEmpty) null
+        else collapseRelativeTokens(path.tail, result.init)
+      } else collapseRelativeTokens(path.tail, result :+ path.head)
+    }
+
     // 1. tokens
     val tokens: List[String] = path.substring(1).split(Directory.SEPARATOR).toList
 
+    // 1.5 eliminate/collapse relative tokens
+
+    /*
+      ["a", "."] => ["a"]
+      ["a", "b", ".", "."] => ["a", "b"]
+
+      /a/../ => ["a", ".."] => []
+      /a/b/.. => ["a", "b", ".."] => ["a"]
+
+     */
+
+    val newTokens = collapseRelativeTokens(tokens, List())
+
     // 2. navigate to the correct entry
-    findEntryHelper(root, tokens)
+    if (newTokens == null) null
+    else findEntryHelper(root, newTokens)
   }
 }
