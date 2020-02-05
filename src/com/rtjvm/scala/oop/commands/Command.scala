4d0135b4aa1f02052c8204305b321f5fbaf1ed8e
Arthur Novaes <arthurnovaes9@gmail.com>
2020-02-05 16:19:58 -0300

    feat: add rm command
    Signed-off-by: Arthur Novaes <arthurnovaes9@gmail.com>

--- src/com/rtjvm/scala/oop/commands/Command.scala
+++ src/com/rtjvm/scala/oop/commands/Command.scala
@@ -13,6 +13,7 @@ object Command {
   val PWD = "pwd"
   val TOUCH = "touch"
   val CD = "cd"
+  val RM = "rm"
 
   def emptyCommand: Command = new Command {
     override def apply(state: State): State = state
@@ -38,8 +39,11 @@ object Command {
       if (tokens.length < 2) incompleteCommand(TOUCH)
       else new Touch(tokens(1))
     } else if (CD.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(MKDIR)
+      if (tokens.length < 2) incompleteCommand(CD)
       else new Cd(tokens(1))
+    } else if (RM.equals(tokens(0))) {
+      if (tokens.length < 2) incompleteCommand(RM)
+      else new Rm(tokens(1))
     }
     else new UnknownCommand
   }
