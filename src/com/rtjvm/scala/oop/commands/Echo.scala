8fb4a50968b523adf6f36186b48e652e95fdfcf9
Arthur Novaes <arthurnovaes9@gmail.com>
2020-02-10 17:10:29 -0300

    feat: add set and append contents on echo command
    Signed-off-by: Arthur Novaes <arthurnovaes9@gmail.com>

--- src/com/rtjvm/scala/oop/commands/Echo.scala
+++ src/com/rtjvm/scala/oop/commands/Echo.scala
@@ -1,4 +1,5 @@
 package com.rtjvm.scala.oop.commands
+import com.rtjvm.scala.oop.files.{Directory, File}
 import com.rtjvm.scala.oop.filesystem.State
 
 import scala.annotation.tailrec
@@ -36,8 +37,37 @@ class Echo(args: Array[String]) extends Command {
     }
   }
 
-  def doEcho(state: State, content: String, filename: String, append: Boolean): State = {
-    ???
+  def getRootAfterEcho(currentDirectory: Directory, path: List[String], contents: String, append: Boolean): Directory = {
+    if (path.isEmpty) currentDirectory
+    else if (path.tail.isEmpty) {
+      val dirEntry = currentDirectory.findEntry(path.head)
+
+      if (dirEntry == null)
+        currentDirectory.addEntry(new File(currentDirectory.path, path.head, contents))
+      else if (dirEntry.isDirectory) currentDirectory
+      else {
+        if (append) currentDirectory.replaceEntry(path.head, dirEntry.asFile.appendContents(contents))
+        else currentDirectory.replaceEntry(path.head, dirEntry.asFile.setContents(contents))
+      }
+    } else {
+      val nextDirectory = currentDirectory.findEntry(path.head).asDirectory
+      val newNextDirectory = getRootAfterEcho(nextDirectory, path.tail, contents, append)
+
+      if (newNextDirectory == nextDirectory) currentDirectory
+      else currentDirectory.replaceEntry(path.head, newNextDirectory)
+    }
+  }
+
+  def doEcho(state: State, contents: String, filename: String, append: Boolean): State = {
+    if (filename.contains(Directory.SEPARATOR))
+      state.setMessage("Echo: filename must not contain separators")
+    else {
+      val newRoot: Directory = getRootAfterEcho(state.root, state.wd.getAllFoldersInPath :+ filename, contents, append)
+      if (newRoot == state.root)
+        state.setMessage(filename + " : no such file")
+      else
+        State(newRoot, newRoot.findDescendant(state.wd.getAllFoldersInPath))
+    }
   }
 
   // topIndex NON-INCLUSIVE
