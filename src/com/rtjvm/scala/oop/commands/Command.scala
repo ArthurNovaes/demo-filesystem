80a1b78d52a03301feb91591d3cfe480a97a6600
Arthur Novaes <arthurnovaes9@gmail.com>
2020-02-11 15:33:32 -0300

    refactor: change many if/else to pattern matching refactor: use functions on command.scala and remove var on FileSystem.scala
    Signed-off-by: Arthur Novaes <arthurnovaes9@gmail.com>

--- src/com/rtjvm/scala/oop/commands/Command.scala
+++ src/com/rtjvm/scala/oop/commands/Command.scala
@@ -2,9 +2,7 @@ package com.rtjvm.scala.oop.commands
 
 import com.rtjvm.scala.oop.filesystem.State
 
-trait Command {
-
-  def apply(state: State): State
+trait Command extends (State => State) {
 }
 
 object Command {
@@ -30,26 +28,31 @@ object Command {
     val tokens: Array[String] = input.split(" ")
 
     if (input.isEmpty || tokens.isEmpty) emptyCommand
-    else if (MKDIR.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(MKDIR)
-      else new Mkdir(tokens(1))
-    } else if (LS.equals(tokens(0))) {
-      new Ls
-    } else if (PWD.equals(tokens(0)))  {
-      new Pwd
-    } else if (TOUCH.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(TOUCH)
-      else new Touch(tokens(1))
-    } else if (CD.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(CD)
-      else new Cd(tokens(1))
-    } else if (RM.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(RM)
-      else new Rm(tokens(1))
-    } else if (ECHO.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(ECHO)
-      else new Echo(tokens.tail)
+    else tokens(0) match {
+      case MKDIR =>
+        if (tokens.length < 2) incompleteCommand(MKDIR)
+        else new Mkdir(tokens(1))
+      case LS =>
+        new Ls
+      case PWD =>
+        new Pwd
+      case TOUCH =>
+        if (tokens.length < 2) incompleteCommand(TOUCH)
+        else new Touch(tokens(1))
+      case CD =>
+        if (tokens.length < 2) incompleteCommand(CD)
+        else new Cd(tokens(1))
+      case RM =>
+        if (tokens.length < 2) incompleteCommand(RM)
+        else new Rm(tokens(1))
+      case ECHO =>
+        if (tokens.length < 2) incompleteCommand(ECHO)
+        else new Echo(tokens.tail)
+      case CAT =>
+        if (tokens.length < 2) incompleteCommand(CAT)
+        else new Cat(tokens(1))
+      case _ =>
+        new UnknownCommand
     }
-    else new UnknownCommand
   }
 }
--- src/com/rtjvm/scala/oop/filesystem/FileSystem.scala
+++ src/com/rtjvm/scala/oop/filesystem/FileSystem.scala
@@ -8,12 +8,18 @@ import com.rtjvm.scala.oop.files.Directory
 object FileSystem extends App {
 
   val root = Directory.ROOT
-  var state = State(root, root)
-  val scanner = new Scanner(System.in)
 
-  while(true) {
-    state.show
-    val input = scanner.nextLine()
-    state = Command.from(input).apply(state)
-  }
+  io.Source.stdin.getLines().foldLeft(State(root, root))((currentState, newLine) => {
+    currentState.show
+    Command.from(newLine).apply(currentState)
+  })
+
+//  var state = State(root, root)
+//  val scanner = new Scanner(System.in)
+//
+//  while(true) {
+//    state.show
+//    val input = scanner.nextLine()
+//    state = Command.from(input).apply(state)
+//  }
 }
